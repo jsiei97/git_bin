09052b7b4b4ac46041255f65e5ff3766568b4182
Johan Simonsson <github@fun-tech.se>
2014-02-07 22:36:00 +0100

    A testrunner for the FunTechHouse projects

---
+++ ft_testrunner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -d test/ ]
+then
+    echo "PWD $PWD"
+    ls -lh
+    echo "No dir called test"
+    exit 50
+fi
+
+base=$PWD
+echo $base
+
+for test in $(find -name *.pro | grep -e ^./test/)
+do
+    echo $test
+    dir=$(dirname $test)
+    name=$(basename $test .pro)
+    pushd $dir || exit 60
+    echo $name
+    qmake || exit 61
+    make || exit 62
+    if [ ! -f $name ]
+    then
+        echo "Build error:"
+        echo $test
+        exit 63
+    fi
+
+    ./$name -xunitxml -o $base/xUnit_report_$name.xml || exit 64
+    make distclean
+    popd
+done
+
+exit 0
