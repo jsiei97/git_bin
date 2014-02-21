20bfae87485cc66511daa15b7c81ce4cb1247879
Johan Simonsson <github@fun-tech.se>
2014-02-21 20:27:10 +0100

    The sed does not always has to be ok

--- ft_testrunner.sh
+++ ft_testrunner.sh
@@ -27,7 +27,12 @@ do
 
     # Since Jenkins looks in the base dir, but we compile in the subdirs
     gccOut=tmp_gcc_out.log
-    make $JOBS > $gccOut 2>&1 || exit 62
+    make $JOBS > $gccOut 2>&1 
+    if [ ! $? = 0 ]
+    then
+        cat $gccOut
+        exit 62
+    fi
 
     src=$(grep SOURCES *.pro | sed s/SOURCES//g | tr -d '+' | tr -d '=' | tr -d '\n' | tr -s ' ')
     echo "src list: $src"
@@ -36,7 +41,7 @@ do
         #But will this work for files not in this test dir?
         to=$(echo $dir'/'$s | sed 's/^.\///g' | sed 's/\//\\\//g')
         echo "PATH: $s -> $to"
-        sed -i 's/^'$s'/'$to'/g' $gccOut || exit 64
+        sed -i 's/^'$s'/'$to'/g' $gccOut #|| exit 64
     done
 
     cat $gccOut
